32b5d391bd04a03039cb77f524c2b177766fc723
Eduardo Flores <eduflormir@gmail.com>
2015-11-26 14:25:49 +0100

    Proyecto Base para servicios que seran implementados por un API REST Funcionara como Wrapper IServiciosRest ServicioRestImpl

---
+++ BaseServicios/BaseServicios/IServiciosRest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BaseServicios
+{
+    public interface IServiciosRest<TModelo>
+    {
+        // operaciones para webservice con tareas asincronas
+
+        Task<TModelo> Add(TModelo model);
+
+        Task Update(TModelo model);
+
+        Task Delete (TModelo model);
+
+        List<TModelo> Get();
+
+        List<TModelo> Get(Dictionary<String,String> param );
+
+        TModelo Get(int id);
+        
+    }
+}
---
+++ BaseServicios/BaseServicios/ServicioRestImpl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace BaseServicios
+{
+    public class ServicioRestImpl<TModelo>: IServiciosRest<TModelo>
+    {
+        private String url; // url donde esta el servicio
+        private bool auth; // indicar si este servicio esta autenticado o no
+        private String user; // para indicar cual es el user
+        private String pass; // para indicar cual es el pwd
+        
+        public ServicioRestImpl(String url, bool auth = false, String user = null, String pass = null)
+        {
+            this.url = url;
+            this.auth = auth;
+            this.user = user;
+            this.pass = pass;
+        }
+
+
+        public Task<TModelo> Add(TModelo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(TModelo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TModelo> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TModelo Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TModelo> Get(Dictionary<string, string> param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TModelo model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
